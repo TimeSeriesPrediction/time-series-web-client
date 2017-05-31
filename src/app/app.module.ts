5e725b92ad5b661c7ff7b7c66a60249341afd408
Claudio Da Silva <Claudio Da Silva>
2017-06-01 01:37:01 +0200

    Added angular material as a dependency to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
