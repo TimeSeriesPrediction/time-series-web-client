27addb0f63ec5549d4c55288e66d371eaa73e00d
Dedre Olwage <dedreolwage@gmail.com>
2017-07-12 10:56:17 +0200

    Replace temporary logo with our main logo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,12 +18,14 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
+import { MarksComponent } from './marks/marks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    MarksComponent
   ],
   imports: [
     BrowserModule,
