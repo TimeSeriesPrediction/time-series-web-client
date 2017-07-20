5f60b134f4536810dd3b1f9461383a7dc51bf881
Murray Le Roux <murray@avantgardeweb.co.uk>
2017-07-20 16:57:06 +0200

    Removed test code accidentally checked in

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
 
 import { AuthService } from '../services/auth-service/auth.service';
-import { AssessmentsApi } from '../services/api-service/assessments-api/assessments.api.mock';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,12 +10,9 @@ import { AssessmentsApi } from '../services/api-service/assessments-api/assessme
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private router: Router, private authService: AuthService, private assessments : AssessmentsApi) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
-    this.assessments.getAssessmentsByModule(1).subscribe((response) => {
-      console.log("Reached here: ", response);
-    });
   }
 
   logout() {
