75652e08a14a233b490954a08b03b8386d540ccc
Merissa Joubert <robogirl1996@gmail.com>
2017-07-26 15:20:39 +0200

    Removed unused code

--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -15,46 +15,6 @@ import * as moment from 'moment';
 
 const Moment: any = (<any>moment).default || moment;
 
-export interface IDateModel {
-  day: string;
-  month: string;
-  year: string;
-  formatted: string;
-  momentObj: moment.Moment;
-}
-
-export class DateModel {
-  day: string;
-  month: string;
-  year: string;
-  formatted: string;
-  momentObj: moment.Moment;
-
-  constructor(obj?: IDateModel) {
-    this.day = obj && obj.day ? obj.day : null;
-    this.month = obj && obj.month ? obj.month : null;
-    this.year = obj && obj.year ? obj.year : null;
-    this.formatted = obj && obj.formatted ? obj.formatted : null;
-    this.momentObj = obj && obj.momentObj ? obj.momentObj : null;
-  }
-}
-
-export interface IDatePickerOptions {
-  autoApply?: boolean;
-  style?: 'normal' | 'big' | 'bold';
-  locale?: string;
-  minDate?: Date;
-  maxDate?: Date;
-  initialDate?: Date;
-  firstWeekdaySunday?: boolean;
-  format?: string;
-  selectYearText?: string;
-  todayText?: string;
-  clearText?: string;
-}
-
-
-
 
 @Component({
   selector: 'app-assignments',
@@ -63,12 +23,7 @@ export interface IDatePickerOptions {
 })
 
 export class AssignmentsComponent {
-
-  @Input() inputEvents: EventEmitter<{ type: string, data: string | DateModel }>;
-  @Output() outputEvents: EventEmitter<{ type: string, data: string | DateModel }>;
-
  
-
   minDate: moment.Moment | any;
   maxDate: moment.Moment | any;
 
