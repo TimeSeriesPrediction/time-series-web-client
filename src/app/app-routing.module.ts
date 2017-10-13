b2c0203d76a3a5d082ad4152e72e91a0e208c045
Merissa Joubert <robogirl1996@gmail.com>
2017-10-13 04:26:41 +0200

    added the email input for password reset with email functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,13 @@
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-
 import {WeightingsComponent} from './components/weightings/weightings.component';
 import {GradingComponent} from './components/grading/grading.component';
 import {AssignmentsComponent} from './components/assignments/assignments.component';
-
-
 import { AdminMarksInterfaceComponent } from './components/admin-marks-interface/admin-marks-interface.component';
-
-
-
 import { StudentQueryComponent} from './components/student-query/student-query.component';
-
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-
 import { MarksComponent } from './components/marks/marks.component';
 import { StudentDashComponent} from './components/student-dash/student-dash.component';
 import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
@@ -26,7 +16,7 @@ import { AdminQueryComponent } from './components/admin-query/admin-query.compon
 import { RedComponentComponent } from './components/red-component/red-component.component';
 import {AddBulkUsersComponent } from './components/add-bulk-users/add-bulk-users.component';
 import { EnrollStudentsComponent } from './components/enroll-students/enroll-students.component';
-
+import {PasswordResetEmailComponent} from './components/password-reset-email/password-reset-email.component';
 const routes: Routes = [
   {
     path: 'login',
@@ -93,6 +83,10 @@ const routes: Routes = [
   {
     path: 'enroll-students',
     component: EnrollStudentsComponent
+  },
+  {
+    path: 'password-reset-email',
+    component: PasswordResetEmailComponent
   }
 ];
 
--- src/app/components/password-reset-email/password-reset-email.component.ts
+++ src/app/components/password-reset-email/password-reset-email.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AuthService} from '../../services/auth-service/auth.service';
 @Component({
   selector: 'app-password-reset-email',
   templateUrl: './password-reset-email.component.html',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordResetEmailComponent implements OnInit {
 
-  constructor() { }
+
+  userEmail : string ;
+  constructor(private authService : AuthService) { }
 
   ngOnInit() {
+  }
+
+  getEmail()
+  {
+    this.authService.requestPasswordReset(this.userEmail);
   }
 
 }
